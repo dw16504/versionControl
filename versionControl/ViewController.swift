b5c555db9640e8f1f157bb8a741535a073cac790
= <douglasdwebb@msn.com>
2025-08-12 12:08:00 -0400

    Here are the comment for the second commit.

--- versionControl/ViewController.swift
+++ versionControl/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        theLabel.text = "This is corrected"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var theLabel: UILabel!
+    
 }
 
